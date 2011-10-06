8781b8fd3dc241dfdc0614c7667fb8ef5304e575
Shawn Bower <shawn.bower@gmail.com>
2011-10-05 22:19:46 -0400

    adding pg

--- app.js
+++ app.js
@@ -29,6 +29,16 @@ app.configure('production', function(){
   app.use(express.errorHandler()); 
 });
 
+var pg = require('pg');
+
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  var query = client.query('SELECT * FROM your_table');
+
+  query.on('row', function(row) {
+    console.log(JSON.stringify(row));
+  });
+});
+
 // Routes
 
 app.get('/', function(req, res){
